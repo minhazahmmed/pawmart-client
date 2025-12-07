be93a7a3c918a373abe9a560043c004dc1ec378d
minhazahmmed <minhazahmmed231@gmail.com>
2025-12-08 00:58:14 +0600

    code formatted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import Slider from '../component/Slider';
-import PopularSection from '../component/PopularSection';
-import WhyAdopt from '../component/WhyAdopt'
-import PetHeroes from '../component/PetHeroes';
-
+import React from "react";
+import Slider from "../component/Slider";
+import PopularSection from "../component/PopularSection";
+import WhyAdopt from "../component/WhyAdopt";
+import PetHeroes from "../component/PetHeroes";
 
 const Home = () => {
-    return (
-        <div>
-            <title>Home</title>
-           <Slider/>
-           <PopularSection/>
-
-           <WhyAdopt/>
-           
-           <PetHeroes/>
-        </div>
-    );
+  return (
+    <div>
+      <title>Home</title>
+      <Slider />
+      <PopularSection />
+      <WhyAdopt />
+      <PetHeroes />
+    </div>
+  );
 };
-
 
 export default Home;
